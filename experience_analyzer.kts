3d10a6b9739deb9c31b15f95d29ac9cd44765fdd
Daniel Orr <orrdaniel73@gmail.com>
2023-10-17 08:55:54 +0100

    Time execution of analyzer

--- experience_analyzer.kts
+++ experience_analyzer.kts
@@ -5,13 +5,14 @@ import java.io.File
 import java.net.URI
 import java.nio.file.Path
 import java.util.UUID
+import kotlin.time.measureTimedValue
 
 // setup
 
 println("Analyzing experience files...")
 
 val playerDataDirectory = Path.of("player_data").toFile()
-val persistentDirectory: File = playerDataDirectory.resolve("experience")
+val experienceDirectory: File = playerDataDirectory.resolve("experience")
 
 val usercacheFile = Path.of("usercache.json").toFile()
 
@@ -37,34 +38,54 @@ val experienceMap = mutableMapOf<String, ExperienceData>()
 // create map
 println("Walking experience directory")
 
-val files: Array<File> = persistentDirectory.listFiles()!!
+val files: Array<File> = experienceDirectory.listFiles()!!
 val size = files.size
+var runningAverageMs = -1L
 files.forEachIndexed { index, file ->
-    try {
-        val uuid: String = UUID.fromString(file.nameWithoutExtension).toString()
-        val name =
-            if (usercacheJson.has(uuid)) {
-                // get username from cache
-                usercacheJson[uuid].asString
-            } else {
-                try {
-                    // lookup username
-                    val lookup = URI("https://playerdb.co/api/player/minecraft/$uuid").toURL().readText()
-                    val lookupJson = JsonParser.parseString(lookup) as JsonObject
-                    val name = lookupJson["data"].asJsonObject["player"].asJsonObject["username"].asString
-                    println("Fetched username of $uuid as $name ($index/$size)")
-                    usercacheJson.addProperty(uuid, name)
-                    usercacheFile.writeText(Gson().toJson(usercacheJson))
-                    name
-                } catch (_: Exception) {
-                    "Unknown"
+    val executionTime = measureTimedValue {
+        try {
+            val uuid: String = UUID.fromString(file.nameWithoutExtension).toString()
+            val name =
+                if (usercacheJson.has(uuid)) {
+                    // get username from cache
+                    usercacheJson[uuid].asString
+                } else {
+                    try {
+                        // lookup username
+                        val lookup = URI("https://playerdb.co/api/player/minecraft/$uuid").toURL().readText()
+                        val lookupJson = JsonParser.parseString(lookup) as JsonObject
+                        val name = lookupJson["data"].asJsonObject["player"].asJsonObject["username"].asString
+                        println("Fetched username of $uuid as $name ($index/$size)")
+                        usercacheJson.addProperty(uuid, name)
+                        usercacheFile.writeText(Gson().toJson(usercacheJson))
+                        name
+                    } catch (_: Exception) {
+                        "Unknown"
+                    }
                 }
-            }
 
-        val json = file.reader().use(JsonParser::parseReader) as JsonObject
-        val experience = ExperienceData(json["total_experience"].asInt, json["level_experience"].asInt, json["level"].asInt + 1)
-        experienceMap[name] = experience
-    } catch (_: Exception) {
+            val json = file.reader().use(JsonParser::parseReader) as JsonObject
+            val experience =
+                ExperienceData(json["total_experience"].asInt, json["level_experience"].asInt, json["level"].asInt + 1)
+            experienceMap[name] = experience
+        } catch (_: Exception) {
+        }
+    }
+
+    val executionTimeMs = executionTime.duration.inWholeMilliseconds
+    runningAverageMs = if (runningAverageMs == -1L) {
+        executionTimeMs
+    } else {
+        (runningAverageMs + executionTimeMs) / 2
+    }
+
+    if (index % 100 == 0) {
+        val remaining = size - index
+        val timeRemaining = (runningAverageMs * remaining) / 1000
+
+        if (timeRemaining != 0L) {
+            println("Time remaining: ${timeRemaining}s")
+        }
     }
 }
 
